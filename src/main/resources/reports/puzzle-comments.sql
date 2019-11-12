b40a8fdcd7c85a71be945ed1b35db090557b88d0
Igorocky <Igorocky@users.noreply.github.com>
2019-11-13 00:43:19 +0100

    Improve puzzle-comments.sql: move FENs to the bottom.

--- src/main/resources/reports/puzzle-comments.sql
+++ src/main/resources/reports/puzzle-comments.sql
@@ -1,11 +1,10 @@
 select comment from (
-    select value comment, rand() ord
+    select value comment,
+    case when REGEXP_LIKE(value, '^([prnbkqPRNBKQ1-8]{1,8}/){7}([prnbkqPRNBKQ1-8]{1,8}) [wb] [-KQkq]{1,4} [-a-h1-8]{1,2} \d+ \d+$') then 2 else rand() end ord
     from tag
     where tag_id = 'chess_puzzle_comment_text'
 )
 order by ord
-
-
 
 /*columns
 [
