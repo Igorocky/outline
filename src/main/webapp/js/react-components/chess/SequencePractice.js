0142feadebffac98811e33bb22cc5143b216e489
Igorocky <Igorocky@users.noreply.github.com>
2019-12-15 15:13:34 +0100

    Improve messages on SequencePractice.js.

--- src/main/webapp/js/react-components/chess/SequencePractice.js
+++ src/main/webapp/js/react-components/chess/SequencePractice.js
@@ -5,17 +5,34 @@ const SequencePractice = ({backend, waitingForNextMove, colorToMove, incorrectMo
     function renderNextActionDescription() {
         if (waitingForNextMove) {
             return RE.Container.row.left.center({},{},
-                "Make next move for " + colorToMove + ".",
+                colorToMove + " to move.",
                 RE.Button({onClick: () => backend.call("showCorrectMove", {})}, "Show move")
             )
         } else {
-            return failed?"":"PASSED"
+            return null
         }
     }
 
+    function getOverallStatus() {
+        if (failed) {
+            return RE.span({style:{color:waitingForNextMove?"orange":"red", fontWeight:"bold"}},"FAILED")
+        } else if (!waitingForNextMove) {
+            return RE.span({style:{color:"limegreen", fontWeight:"bold"}},"PASSED")
+        } else {
+            return null
+        }
+    }
+
+    function renderProgressStatus() {
+        return RE.Container.row.left.center({},{},
+            RE.span({},waitingForNextMove?"In progress... ":"Completed: "),
+            getOverallStatus()
+        )
+    }
+
     return RE.Container.col.top.left({},{},
-        failed?"FAILED":"",
+        renderProgressStatus(),
         renderNextActionDescription(),
-        incorrectMove?"Incorrect move!":"",
+        RE.span({style:{color:"red"}},incorrectMove?"Incorrect move!":""),
     )
 }
