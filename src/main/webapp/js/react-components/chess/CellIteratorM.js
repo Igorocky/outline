02f68347bb1ca1f05c2023852d51bcbffd911f1d
Igorocky <Igorocky@users.noreply.github.com>
2020-02-29 21:18:53 +0100

    Hide CellIterator if it is not needed.

--- src/main/webapp/js/react-components/chess/CellIteratorM.js
+++ src/main/webapp/js/react-components/chess/CellIteratorM.js
@@ -4,9 +4,7 @@ const CellIteratorM = ({numberOfPieces, quiz}) => {
     const [idx, setIdx] = useState(-1)
 
     function nextClicked() {
-        if (idx < quiz.length) {
-            setIdx(oldIdx => oldIdx+1)
-        }
+        setIdx(oldIdx => oldIdx+1)
     }
 
     function prevClicked() {
@@ -15,17 +13,21 @@ const CellIteratorM = ({numberOfPieces, quiz}) => {
         }
     }
 
-    return RE.Container.row.right.bottom({}, {style:{marginLeft:"1em"}},
-        RE.span({style: {fontSize: "large"}},
-            idx == -1
-                ? "Start"
-                : (idx < quiz.length ? (quiz[idx].question + " : " + quiz[idx].answer) : numberOfPieces)
-        ),
-        RE.Paper({},
-            RE.Container.col.top.right({},{},
-                iconButton({iconName: "arrow_back_ios", onClick: prevClicked}),
-                iconButton({iconName: "arrow_forward_ios", onClick: nextClicked}),
+    if (idx > quiz.length) {
+        return RE.Button({onClick: () => setIdx(0)}, numberOfPieces)
+    } else {
+        return RE.Container.row.right.bottom({}, {style:{marginLeft:"1em"}},
+            RE.span({style: {fontSize: "large"}},
+                idx == -1
+                    ? "Start"
+                    : (idx < quiz.length ? (quiz[idx].question + " : " + quiz[idx].answer) : numberOfPieces)
+            ),
+            RE.Paper({},
+                RE.Container.col.top.right({},{},
+                    iconButton({iconName: "arrow_back_ios", onClick: prevClicked}),
+                    iconButton({iconName: "arrow_forward_ios", onClick: nextClicked}),
+                )
             )
         )
-    )
+    }
 }
